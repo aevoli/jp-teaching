b4059555ad67607016050764273f8e949d3134a2
Joshua Burns <jburns9899@outlook.com>
2026-02-19 21:19:00 +0000

    add 6 more animals to animal sounds (duck, horse, sheep, lion, monkey, bear)

--- jp-teaching-app/lib/data/band-a.ts
+++ jp-teaching-app/lib/data/band-a.ts
@@ -5,6 +5,12 @@ export const animals = [
   { en: "Frog", jp: "かえる", romaji: "Kaeru", sound: "ケロケロ", soundRomaji: "Kero-kero", emoji: "🐸" },
   { en: "Cow", jp: "うし", romaji: "Ushi", sound: "モーモー", soundRomaji: "Moo-moo", emoji: "🐄" },
   { en: "Pig", jp: "ぶた", romaji: "Buta", sound: "ブーブー", soundRomaji: "Buu-buu", emoji: "🐷" },
+  { en: "Duck", jp: "あひる", romaji: "Ahiru", sound: "ガーガー", soundRomaji: "Gaa-gaa", emoji: "🦆" },
+  { en: "Horse", jp: "うま", romaji: "Uma", sound: "ヒヒーン", soundRomaji: "Hihiin", emoji: "🐴" },
+  { en: "Sheep", jp: "ひつじ", romaji: "Hitsuji", sound: "メーメー", soundRomaji: "Mee-mee", emoji: "🐑" },
+  { en: "Lion", jp: "らいおん", romaji: "Raion", sound: "ガオー", soundRomaji: "Gaoo", emoji: "🦁" },
+  { en: "Monkey", jp: "さる", romaji: "Saru", sound: "キーキー", soundRomaji: "Kii-kii", emoji: "🐒" },
+  { en: "Bear", jp: "くま", romaji: "Kuma", sound: "ガオガオ", soundRomaji: "Gao-gao", emoji: "🐻" },
 ];
 
 export const colours = [
